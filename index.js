07db1a27438134d57d840eba53a2c26aa1cb8591
ashu rajput <rajputashu130@gmail.com>
2025-09-19 15:38:59 +0530

    dome

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOption = {
-  origin: 'https://job-portal-frontend-d38l.vercel.app/',
+  origin: 'https://job-portal-frontend-six-lake.vercel.app/',
   credentials: true 
 };
 
